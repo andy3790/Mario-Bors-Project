4cf75d4b8caba3532df77139a941cee0db7d4df5
andy3790 <parkyj477@gmail.com>
2021-12-07 04:16:14 +0900

    무한 스크롤링 맵

--- map_object.py
+++ map_object.py
@@ -1,21 +1,39 @@
 from pico2d import *
 
+import server
+
 # Game object class here
 class Map_BackGround:
+
     def __init__(self):
         self.image = load_image('image/1-1_background.png')
-        self.x = 400
-        self.y = 300
+        self.canvas_width = get_canvas_width()
+        self.canvas_height = get_canvas_height()
+        self.w = self.image.w
+        self.h = self.image.h
 
     def get_bb(self):
         return 0, 0, 800, 35
 
-    def update(self):
-        pass
-
     def draw(self):
-        self.image.clip_draw(0, 0, 792, 792, self.x , self.y,800,600)
-        draw_rectangle(*self.get_bb())
+        self.image.clip_draw_to_origin(self.q1l, self.q1b, self.q1w, self.q1h, 0, 0)                        # quadrant 1
+        self.image.clip_draw_to_origin(self.q2l, self.q2b, self.q2w, self.q2h, self.q1w, 0)                 # quadrant 2
+
+    def update(self):
+        # quadrant 3
+        self.q1l = (int(server.mario.x) - self.canvas_width // 2) % self.w
+        self.q1b = 0
+        self.q1w = clamp(0, self.w - self.q1l, self.w)
+        self.q1h = self.canvas_height
+        # quadrand 4
+        self.q2l = 0
+        self.q2b = self.q1b
+        self.q2w = self.canvas_width - self.q1w
+        self.q2h = self.q1h
+
+
+    def handle_event(self, event):
+        pass
 
 
 class Item_Box:
